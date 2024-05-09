637b3b475e09ad0919faf191304aa00d83df7d6b
Jean Augusto <jean88asl@gmail.com>
2024-05-08 22:05:17 -0300

    added error handling

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,14 @@
+const AppError = require("../utils/App.Error")
+
 class UsersController {
     create(request, response) {
         const { name, age } = request.body
 
-        response.json({name, age})
+        if(!name){
+            throw new AppError("Nome é obrigatório!")
+        }
+
+        response.status(201).json({name, age})
     }
 }
 module.exports = UsersController
--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors")
+
+const AppError = require("./utils/App.Error")
 const express = require("express")
 const routes = require("./routes")
 
@@ -5,6 +8,22 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+    // logando erro no console
+    console.log(error)
+    // logando erro no nosso lado
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+})
 
 const PORT = 3333
 app.listen(PORT, () => {
