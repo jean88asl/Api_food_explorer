fadbdac00cdc2264ba54adb639ea373569c5a975
Jean Augusto <jean88asl@gmail.com>
2024-07-02 16:42:37 -0300

    added user creation method

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,15 +1,34 @@
+const knex = require('../database/knex')
+const { hash } = require('bcryptjs')
 const AppError = require("../utils/App.Error")
 
 class UsersController {
-    create(request, response) {
-        const { name, age } = request.body
+    async create(request, response) {
+        const { name, email, password } = request.body
 
-        if(!name){
-            throw new AppError("Nome é obrigatório!")
+        if (!name || !email || !password) {
+            throw new AppError("Todos os dados são obrigatórios!")
         }
 
-        response.status(201).json({name, age})
+        const userExist = await knex("users").where({ email }).first()
+
+        if (userExist) {
+            throw new AppError("Email já cadastrado!")
+        }
+
+        const hashedPassword = await hash(password, 8)
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword,
+        }
+
+        await knex("users").insert(user)
+
+        response.status(201).json("dados gravados com sucesso")
     }
 }
+
 module.exports = UsersController
 
