b24ed1786d0366f46f9036abeb74034545dc2cd9
Jean Augusto <jean88asl@gmail.com>
2024-07-04 17:19:59 -0300

    users middleware included

--- src/controllers/UserRequestsController.js
+++ src/controllers/UserRequestsController.js
@@ -1,7 +1,10 @@
 class UserRequestsController {
     create(request, response) {
-        const {list, total, type_payment} = request.body
+        const { list, total, type_payment } = request.body
 
+        //pegando o id do usuário no cabeçalho da requisição 
+        const user_id = request.user.id
+        
         const requestUser = {
             list,
             total,
@@ -10,6 +13,6 @@ class UserRequestsController {
 
         response.json(requestUser)
     }
-} 
+}
 
 module.exports = UserRequestsController
--- src/routes/userRequests.routes.js
+++ src/routes/userRequests.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express")
 
 const UserRequestsController = require("../controllers/UserRequestsController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const userRequestsRoutes = Router()
 const userRequests = new UserRequestsController()
 
-userRequestsRoutes.post("/", userRequests.create)
+userRequestsRoutes.post("/", ensureAuthenticated, userRequests.create)
 
 module.exports = userRequestsRoutes
