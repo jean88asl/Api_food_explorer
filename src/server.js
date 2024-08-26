469b3d7a4cb52a9686197f5a62c2872f0bc007ee
Jean Augusto <jean88asl@gmail.com>
2024-08-26 19:37:40 -0300

    test port

--- src/server.js
+++ src/server.js
@@ -33,5 +33,5 @@ app.use((error, request, response, next) => {
     })
 })
 
-const PORT = process.env.SERVER_PORT || 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Acesse aqui http://localhost.com:${PORT}`))
